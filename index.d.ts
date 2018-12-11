8739ae80336dc00a373798b3cb79534382ccad45
Roman Samec <roman.samec@oracle.com>
2018-12-11 11:44:19 +0100

    fix module name

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "ko-component-tester" {
+declare module "ko-component-tester-fork" {
     export function renderComponent(component: any, params?: any, bindingContext?: any):JQuery;
     export function renderHtml(component: any, params?: any, bindingContext?: any):JQuery;
 }
